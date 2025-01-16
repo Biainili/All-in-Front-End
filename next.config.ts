aaef0798250cbe0229c767f150f7ce0900fb0071
Biainili <barkhudaryan005@gmail.com>
2025-01-16 23:45:06 +0300

    First Commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Включает строгий режим React
+  swcMinify: true, // Включает минимизацию через SWC
+  images: {
+    unoptimized: true, // Требуется, если вы используете Next.js без встроенного оптимизатора изображений на Netlify
+  },
+  trailingSlash: true, // Создает HTML-страницы для всех маршрутов (например, `/about` → `/about/index.html`)
+  output: "standalone", // Позволяет Netlify обрабатывать приложение в серверной среде
 };
 
-export default nextConfig;
+module.exports = nextConfig;
